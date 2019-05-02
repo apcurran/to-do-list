9021db36c932c4c456fb7afd6d64e75695fd8b8f
apc <alexcurran.va@gmail.com>
2019-05-02 12:08:04 -0500

    Added delete button functionality for items

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let items = document.querySelectorAll(".item");
-console.log(items);
+// Delete buttons for items
+
+let btns = document.querySelectorAll(".delete");
+console.log(btns);
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+       const li = event.target.parentElement;
+       console.log(li);
+       li.parentElement.removeChild(li);
+    })
+});
+
